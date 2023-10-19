1ea2da1bc0e0081217ccd4d14c15d468aaa2ccd1
abdulrehmankz1 <khanadaabdulrehman1@gmail.com>
2023-10-19 12:28:46 +0500

    fixed some issue.

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     const divs = document.querySelectorAll(".grid");
-//     const prevBtn = document.getElementById("prevBtn");
-//     const nextBtn = document.getElementById("nextBtn");
-//     let currentIndex = 0;
+document.addEventListener("DOMContentLoaded", function () {
+    const divs = document.querySelectorAll(".grid");
+    const prevBtn = document.getElementById("prevBtn");
+    const nextBtn = document.getElementById("nextBtn");
+    let currentIndex = 0;
 
-//     function showDiv(index) {
-//         divs.forEach((div, i) => {
-//             if (i === index) {
-//                 div.style.display = "block";
-//             } else {
-//                 div.style.display = "none";
-//             }
-//         });
-//     }
+    function showDiv(index) {
+        divs.forEach((div, i) => {
+            if (i === index) {
+                div.style.display = "block";
+            } else {
+                div.style.display = "none";
+            }
+        });
+    }
 
-//     nextBtn.addEventListener("click", function () {
-//         currentIndex++;
-//         if (currentIndex >= divs.length) {
-//             currentIndex = 0;
-//         }
-//         showDiv(currentIndex);
-//     });
+    nextBtn.addEventListener("click", function () {
+        currentIndex++;
+        if (currentIndex >= divs.length) {
+            currentIndex = 0;
+        }
+        showDiv(currentIndex);
+    });
 
-//     prevBtn.addEventListener("click", function () {
-//         currentIndex--;
-//         if (currentIndex < 0) {
-//             currentIndex = divs.length - 1;
-//         }
-//         showDiv(currentIndex);
-//     });
+    prevBtn.addEventListener("click", function () {
+        currentIndex--;
+        if (currentIndex < 0) {
+            currentIndex = divs.length - 1;
+        }
+        showDiv(currentIndex);
+    });
 
-//     // Initially, show the first div.
-//     showDiv(currentIndex);
-// });
+    // Initially, show the first div.
+    showDiv(currentIndex);
+});
